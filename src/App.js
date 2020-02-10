0f223dbb62d6e73a024a753898734a1b9c605ffc
kenjinakamu <54579641+kenjinakamu@users.noreply.github.com>
2020-02-10 23:08:57 +0900

    show viewer has starred

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import client from './client';
 import { SEARCH_REPOSITORIES } from './graphql';
 
 const StarButton = props => {
-  const totalCount = props.node.stargazers.totalCount;
-return <button>{totalCount === 1 ? "1 star" : `${totalCount} stars`}</button>
+  const node = props.node;
+  const totalCount = node.stargazers.totalCount;
+  const viewerHasStarred = node.viewerHasStarred;
+  const starCount = totalCount === 1 ? "1 star" : `${totalCount} stars`;
+  return (
+    <button>
+      {starCount} | {viewerHasStarred ? 'starred' : '-'}
+    </button>
+  )
 }
 
 const PER_PAGE = 5;
@@ -56,10 +63,10 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <form>
-          <input value={query} onChange={this.handleChange}/>
+          <input value={query} onChange={this.handleChange} />
         </form>
 
-        <Query 
+        <Query
           query={SEARCH_REPOSITORIES}
           variables={{ query, first, last, before, after }}
         >
@@ -67,51 +74,51 @@ class App extends Component {
             ({ loading, error, data }) => {
               if (loading) return 'Loading...'
               if (error) return `Error! ${error.message}`
-  
+
               const search = data.search;
               const repositoryCount = search.repositoryCount;
               const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories';
               const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`
-            return (
-              <React.Fragment>
-                <h2>{title}</h2>
-                <ul>
+              return (
+                <React.Fragment>
+                  <h2>{title}</h2>
+                  <ul>
+                    {
+                      search.edges.map(e => {
+                        const node = e.node;
+                        return (
+                          <li key={node.id}>
+                            <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
+                            &nbsp;
+                          <StarButton node={node} />
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
                   {
-                    search.edges.map(e => {
-                      const node = e.node;
-                      return (
-                        <li key={node.id}>
-                          <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
-                          &nbsp;
-                          <StarButton node={node}/>
-                        </li>
-                      )
-                    })
-                  }
-                </ul>
-                {
-                  search.pageInfo.hasPreviousPage === true ?
-                    <button
-                      onClick={this.goPrevious.bind(this, search)}
-                    >
-                      Previous
+                    search.pageInfo.hasPreviousPage === true ?
+                      <button
+                        onClick={this.goPrevious.bind(this, search)}
+                      >
+                        Previous
                     </button>
-                    :
-                    null
-                }
-                
-                {
-                  search.pageInfo.hasNextPage === true ?
-                  <button
-                    onClick={this.goNext.bind(this, search)}
-                  >
-                    next
+                      :
+                      null
+                  }
+
+                  {
+                    search.pageInfo.hasNextPage === true ?
+                      <button
+                        onClick={this.goNext.bind(this, search)}
+                      >
+                        next
                   </button>
-                  :
-                  null
-                }
-              </React.Fragment>
-            )
+                      :
+                      null
+                  }
+                </React.Fragment>
+              )
             }
           }
         </Query>
