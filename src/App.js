74a8daa9f60fa62c7b911aa318b19e91b1b298bf
kenjinakamu <54579641+kenjinakamu@users.noreply.github.com>
2020-02-12 09:29:29 +0900

    less queries with refs

--- src/App.js
+++ src/App.js
@@ -59,23 +59,26 @@ const DEFAULT_STATE = {
   after: null,
   last: null,
   before: null,
-  query: "フロントエンドエンジニア"
+  query: ""
 }
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = DEFAULT_STATE;
-
-    this.handleChange = this.handleChange.bind(this);
+    
+    this.myRef = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+  handleSubmit(event) {
+    event.preventDefault()
+
     this.setState({
-      ...DEFAULT_STATE,
-      query: event.target.value
+      query: this.myRef.current.value
     })
   }
+  
 
   goPrevious(search) {
     this.setState({
@@ -99,8 +102,9 @@ class App extends Component {
     const { query, first, last, before, after } = this.state;
     return (
       <ApolloProvider client={client}>
-        <form>
-          <input value={query} onChange={this.handleChange} />
+        <form onSubmit={this.handleSubmit}>
+          <input ref={this.myRef}/>
+          <input type="submit" value="Submit"/>
         </form>
 
         <Query
